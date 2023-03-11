b022d4d058c1aa2a17cd80c1ba6a4787233b2547
chris0128 <22466967@life.hkbu.edu.hk>
2023-03-11 17:56:23 +0800

    update - New act message

--- python bot - update.py
+++ python bot - update.py
@@ -25,6 +25,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello_kevin", hello_kevin_command))
     # To start the bot:
     updater.start_polling()
     updater.idle()
@@ -42,6 +43,11 @@ def echo(update, context):
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Helping you helping you.')
+
+
+def hello_kevin_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /hello_kevin is issued."""
+    update.message.reply_text('Good day, Kevin!')
 
 
 def add(update: Update, context: CallbackContext) -> None:
